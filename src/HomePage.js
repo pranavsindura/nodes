dbc3c451bd7a4f06a8734d1d3878a104ddf2c535
Pranav Sindura <pranav.sindura@gmail.com>
2021-02-25 13:00:44 +0530

    home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,54 @@
 import React from 'react';
-
+import { Container, NavDropdown, Button, Navbar, Form, Card, CardColumns } from 'react-bootstrap';
+import './HomePage.css';
 class HomePage extends React.Component {
 	state = {};
 
 	render() {
-		return <div>Home</div>;
+		let card_list = [];
+		for (let i = 0; i < 10; i++)
+			card_list.push(
+				<Card>
+					<Card.Body>
+						<Card.Title>Card title</Card.Title>
+						<Card.Text>
+							This is a wider card with supporting text below as a natural lead-in to additional content.
+							This content is a little bit longer.
+						</Card.Text>
+					</Card.Body>
+					<Card.Footer>
+						<small className="text-muted">Last updated 3 mins ago</small>
+					</Card.Footer>
+				</Card>
+			);
+
+		return (
+			<Container fluid className="home">
+				<Container>
+					<Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
+						<Navbar.Brand className="nodes-text">NODES | User</Navbar.Brand>
+						<Navbar.Toggle />
+						<Navbar.Collapse>
+							<Form inline>
+								<Form.Control type="input" placeholder="Search" />
+							</Form>
+							<NavDropdown title="Create New" className="nodes-text">
+								<NavDropdown.Item>Note</NavDropdown.Item>
+								<NavDropdown.Item>Checklist</NavDropdown.Item>
+							</NavDropdown>
+							<Button variant="outline-danger" className="ml-auto logout">
+								Logout
+							</Button>
+						</Navbar.Collapse>
+					</Navbar>
+				</Container>
+				<Container>
+					<CardColumns>
+                        {card_list}
+                    </CardColumns>
+				</Container>
+			</Container>
+		);
 	}
 }
 
